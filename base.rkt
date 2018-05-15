62f9e1b47f06cdb33ca1025690504eba15f386bf
Eric Griffis <dedbox@gmail.com>
2018-05-15 16:00:07 -0700

    add some racket/base functions

--- base.rkt
+++ base.rkt
@@ -4,7 +4,37 @@
 
 (require
  event/concurrent
- event/sequential)
+ event/sequential
+ racket/function
+ racket/list)
+
+(define (merge-args Es+Es*)
+  (append (drop-right Es+Es* 1) (last Es+Es*)))
+
+;; Sequential
+
+(define (event-list . Es)
+  (event-list* Es))
+
+(define (event-list* . Es+Es*)
+  (fmap list (args* (merge-args Es+Es*))))
+
+(define (event-map f . Ess)
+  (fmap* (curry map f) (map event-list* Ess)))
+
+;; Concurrent
 
 (define (async-list . Es)
-  (fmap list (async-set* Es)))
+  (async-list* Es))
+
+(define (async-list* . Es+Es*)
+  (fmap list (async-args* (merge-args Es+Es*))))
+
+(define (async-map f . Ess)
+  (async-fmap* (curry map f) (map async-list* Ess)))
+
+(define (async-void . Es)
+  (async-fmap* void Es))
+
+(define (async-void* . Es+Es*)
+  (async-fmap* void (merge-args Es+Es*)))
