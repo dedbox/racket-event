6e2761308e2abe19766ae49ddb678b3ccdadb750
Eric Griffis <dedbox@gmail.com>
2018-05-25 10:07:56 -0700

    fix unit test name indentation

--- sequential.rkt
+++ sequential.rkt
@@ -115,38 +115,32 @@
 
   ;; doc examples
 
-  (test-case
-      "pure"
+  (test-case "pure"
     (define N 0)
     (define evt (pure (set! N (add1 N))))
     (sync evt)
     (sync evt)
     (check = N 2))
 
-  (test-case
-      "return"
+  (test-case "return"
     (define N 0)
     (define evt (return (set! N (add1 N))))
     (sync evt)
     (sync evt)
     (check = N 1))
 
-  (test-case
-      "args"
+  (test-case "args"
     (check equal?
            (sync (handle (args (pure 1) (pure 2) (pure 3)) list))
            '(1 2 3)))
 
-  (test-case
-      "fmap"
+  (test-case "fmap"
     (check = (sync (fmap + (pure 1) (pure 2) (pure 3))) 6))
 
-  (test-case
-      "app"
+  (test-case "app"
     (check = (sync (app (pure +) (pure 1) (pure 2) (pure 3))) 6))
 
-  (test-case
-      "bind"
+  (test-case "bind"
     (check =
            (sync
             (bind
@@ -156,21 +150,17 @@
              (compose return +)))
            6))
 
-  (test-case
-      "seq"
+  (test-case "seq"
     (check = (sync (seq (pure 1) (pure 2) (pure 3))) 3))
 
-  (test-case
-      "seq0"
+  (test-case "seq0"
     (check = (sync (seq0 (pure 1) (pure 2) (pure 3))) 1))
 
-  (test-case
-      "test"
+  (test-case "test"
     (check = (sync (test (pure #t) (pure 1) (pure 2))) 1)
     (check = (sync (test (pure #f) (pure 1) (pure 2))) 2))
 
-  (test-case
-      "series"
+  (test-case "series"
     (check =
            (sync
             (series
@@ -179,8 +169,7 @@
              (compose return (curry * 3))))
            9))
 
-  (test-case
-      "reduce"
+  (test-case "reduce"
     (check =
            (sync
             (reduce
@@ -189,8 +178,7 @@
              0))
            10))
 
-  (test-case
-      "loop"
+  (test-case "loop"
     (check =
            (with-handlers ([number? values])
              (sync (loop (λ (x)
@@ -202,16 +190,14 @@
 
   ;; monad laws
 
-  (test-case
-      "fmap id == id"
+  (test-case "fmap id == id"
     (for ([v 10])
       (check
        = v
        (sync (fmap id (pure v)))
        (sync (id (pure v))))))
 
-  (test-case
-      "fmap (f . g) == fmap f . fmap g"
+  (test-case "fmap (f . g) == fmap f . fmap g"
     (for ([v 10])
       (check
        = (+ 2 (* 3 v))
@@ -220,16 +206,14 @@
                        (λ (x) (fmap (curry * 3) x)))
               (pure v))))))
 
-  (test-case
-      "pure id <*> v = v"
+  (test-case "pure id <*> v = v"
     (for ([v 10])
       (check
        = v
        (sync (app (pure id) (pure v)))
        (sync (pure v)))))
 
-  (test-case
-      "pure (.) <*> u <*> v <*> w = u <*> (v <*> w)"
+  (test-case "pure (.) <*> u <*> v <*> w = u <*> (v <*> w)"
     (define u (pure (curry + 2)))
     (define v (pure (curry * 3)))
     (define w (pure (curry - 1)))
@@ -240,8 +224,7 @@
        (sync (app u (app v (app w (pure i)))))
        )))
 
-  (test-case
-      "pure f <*> pure x = pure (f x)"
+  (test-case "pure f <*> pure x = pure (f x)"
     (define f add1)
     (for ([x 10])
       (check
@@ -249,8 +232,7 @@
        (sync (app (pure f) (pure x)))
        (sync (pure (f x))))))
 
-  (test-case
-      "u <*> pure y = pure ($ y) <*> u"
+  (test-case "u <*> pure y = pure ($ y) <*> u"
     (define u (pure (curry + 2)))
     (for ([y 10])
       (check
@@ -258,8 +240,7 @@
        (sync (app u (pure y)))
        (sync (app (pure (λ (f) (f y))) u)))))
 
-  (test-case
-      "return a >>= k  =  k a"
+  (test-case "return a >>= k  =  k a"
     (define k (λ (x) (pure (add1 x))))
     (for ([a 10])
       (check
@@ -267,16 +248,14 @@
        (sync (bind (return a) k))
        (sync (k a)))))
 
-  (test-case
-      "m >>= return  =  m"
+  (test-case "m >>= return  =  m"
     (for ([m 10])
       (check
        = m
        (sync (bind (pure m) return))
        (sync (pure m)))))
 
-  (test-case
-      "m >>= (\\x -> k x >>= h)  =  (m >>= k) >>= h"
+  (test-case "m >>= (\\x -> k x >>= h)  =  (m >>= k) >>= h"
     (define h (λ (x) (pure (+ 2 x))))
     (define k (λ (x) (pure (* 3 x))))
     (for ([m 10])
@@ -285,35 +264,30 @@
        (sync (bind (pure m) (λ (x) (bind (k x) h))))
        (sync (bind (bind (pure m) k) h)))))
 
-  (test-case
-      "seq"
+  (test-case "seq"
     (for ([v 10])
       (check
        = v
        (sync (seq (pure -1) (pure -2) (pure v))))))
 
-  (test-case
-      "seq0"
+  (test-case "seq0"
     (for ([v 10])
       (check
        = v
        (sync (seq0 (pure v) (pure -2) (pure -3))))))
 
-  (test-case
-      "test"
+  (test-case "test"
     (check = 1 (sync (test (pure #t) (pure 1) (pure 2))))
     (check = 2 (sync (test (pure #f) (pure 1) (pure 2)))))
 
-  (test-case
-      "series"
+  (test-case "series"
     (check
      = 5
      (sync
       (apply series (pure 0)
              (for/list ([_ 5]) (λ (x) (pure (+ x 1))))))))
 
-  (test-case
-      "reduce"
+  (test-case "reduce"
     (check
      = 10
      (sync
@@ -322,8 +296,7 @@
        (λ (x y) (>= y 10))
        0))))
 
-  (test-case
-      "loop"
+  (test-case "loop"
     (check
      = 10
      (with-handlers ([number? values])
