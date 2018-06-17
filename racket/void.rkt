fe86809b65dac11bb393dbe13d7da1d5a983927e
Eric Griffis <dedbox@gmail.com>
2018-06-17 10:36:31 -0700

    add event-void combinator

--- racket/void.rkt
+++ racket/void.rkt
@@ -2,16 +2,25 @@
 
 (require
  event/concurrent
+ event/sequential
  racket/contract/base
  racket/list)
 
 (provide
  (contract-out
+  [event-void (-> evt? ... evt?)]
+  [event-void* (-> evt? ... (listof evt?) evt?)]
   [async-void (-> evt? ... evt?)]
   [async-void* (-> evt? ... (listof evt?) evt?)]))
 
 (define (rest-args Es+Es*)
   (append (drop-right Es+Es* 1) (last Es+Es*)))
+
+(define (event-void . Es)
+  (fmap* void Es))
+
+(define (event-void* . Es+Es*)
+  (fmap* void (rest-args Es+Es*)))
 
 (define (async-void . Es)
   (async-fmap* void Es))
